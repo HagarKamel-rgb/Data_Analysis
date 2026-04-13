13fe964a852b49e286c86f0decf91a6446256630
HagarKamel-rgb <hagarkamel630@gmail.com>
2026-04-13 20:59:34 +0200

    Data Project

---
+++ analysis.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv(r"E:\pythonnn\DataAnalysis\tips.csv")
+print(df.head())
+
+# تنظيف البيانات
+df.dropna(inplace=True)
+
+# تحليل
+print("Total Tips:", df["tip"].sum())
+print("Average Tip:", df["tip"].mean())
+
+print("\nTips by Day:")
+print(df.groupby("day")["tip"].sum())
+
+# رسم
+df.groupby("day")["tip"].sum().plot(kind='bar')
+plt.title("Total Tips by Day")
+plt.show()
+
+df.groupby("sex")["tip"].sum().plot(kind='bar')
+plt.title("Tips by Gender")
+plt.show()
+
+plt.scatter(df["total_bill"], df["tip"])
+plt.title("Total Bill vs Tip")
+plt.xlabel("Total Bill")
+plt.ylabel("Tip")
+plt.show()
